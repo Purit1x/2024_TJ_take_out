188d36b78469a112cc51d7255d51daa19e20d691
Purit1x <xuy_eason@foxmail.com>
2024-07-29 11:06:46 +0800

    change the template of the project

---
+++ takeout_tj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace takeout_tj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
