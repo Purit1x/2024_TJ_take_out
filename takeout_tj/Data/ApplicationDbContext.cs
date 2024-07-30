ce69b2930e6713a4b541a4c5c2e7bf18f959b444
Purit1x <xuy_eason@foxmail.com>
2024-07-30 22:57:08 +0800

    create the class which is inherited from DbContext and modify the appsettings.json to set the default Oracle database connection string.

---
+++ takeout_tj/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Oracle.EntityFrameworkCore;
+
+namespace takeout_tj.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+		{
+		}
+
+
+	}
+}
