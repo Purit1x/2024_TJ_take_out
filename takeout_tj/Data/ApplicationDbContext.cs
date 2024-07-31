58031bf229dbe7eff18953f3f132bb1463b58c9c
Purit1x <xuy_eason@foxmail.com>
2024-07-31 23:01:52 +0800

    Finish some model classes and modify ApplicationsDbContext.

--- takeout_tj/Data/ApplicationDbContext.cs
+++ takeout_tj/Data/ApplicationDbContext.cs
@@ -1,14 +1,47 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Oracle.EntityFrameworkCore;
+using takeout_tj.Models.User;
 
 namespace takeout_tj.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
+		public DbSet<UserDB> Users { get; set; }
+		public DbSet<UserAddressDB> UserAddresses { get; set; }
+		public DbSet<Membership> Memberships { get; set; }
+		
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
 		{
 		}
 
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			if(!optionsBuilder.IsConfigured)
+			{
 
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<UserDB>().HasKey(u => u.UserId);
+			modelBuilder.Entity<UserAddressDB>().HasKey(u => new {u.UserId, u.UserAddress});
+			// 定义地址到用户的多对一关系
+			modelBuilder.Entity<UserAddressDB>()
+				.HasOne(a => a.UserDB)
+				.WithMany()
+				.HasForeignKey(a => a.UserId);
+			modelBuilder.Entity<Membership>().HasKey(m => m.UserId);
+			modelBuilder.Entity<Membership>()
+				.HasOne(m => m.UserDB)
+				.WithOne()
+				.HasForeignKey<Membership>(m => m.UserId);
+				
+			modelBuilder.Entity<UserDB>().ToTable("Users");
+			modelBuilder.Entity<UserAddressDB>().ToTable("UserAddresses");
+			modelBuilder.Entity<Membership>().ToTable("Memberships");
+		}
 	}
 }
---
+++ takeout_tj/Models/User/UserDB.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace takeout_tj.Models.User
+{
+	public class UserDB
+	{
+		[Required(ErrorMessage = "User ID is required.")]
+		public int UserId { get; set; }
+
+		[Required(ErrorMessage = "User name is required.")]
+		[StringLength(50, ErrorMessage = "The field UserName must be at most 15 characters long. ")]
+		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Phone number is required.")]
+		[StringLength(11, MinimumLength = 11, ErrorMessage = "The field PhoneNumber must be exactly 10 digitss. ")]
+		[RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number can only contain digits. ")]
+		public string PhoneNumber {  get; set; }
+
+		[Required(ErrorMessage = "User password is required. ")]
+		[StringLength(20, MinimumLength = 6, ErrorMessage = "The password should be at least 6 characters and at most 20 characters long. ")]
+		public string Password { get; set; }
+	}
+}
