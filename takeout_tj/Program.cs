d900f00b87821a4cec7c09fe6da813f797faf025
JiangShiYu6 <129039574+JiangShiYu6@users.noreply.github.com>
2024-08-04 17:33:07 +0800

    Test_2

--- takeout_tj/Program.cs
+++ takeout_tj/Program.cs
@@ -1,34 +1,90 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using takeout_tj.Data;
+using takeout_tj.Utility;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//全局注册 --对于整个项目中所有的方法都生效的
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add<CustomExceptionFilterAttribute>();
+    opt.Conventions.Insert(0, new RouteConvention(new RouteAttribute("api/")));
+})
+//解决中文乱码问题
+.AddJsonOptions(options =>
+{
+    //取消Unicode编码
+    options.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+    //格式化日期时间格式
+    // options.JsonSerializerOptions.Converters.Add(new DatetimeJsonConverter("yyyy-MM-dd HH:mm:ss"));
+    //数据格式首字母小写
+    // options.JsonSerializerOptions.PropertyNamingPolicy =JsonNamingPolicy.CamelCase;
+    //数据格式原样输出
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    //忽略空值
+    options.JsonSerializerOptions.IgnoreNullValues = true;
+    //允许额外符号
+    options.JsonSerializerOptions.AllowTrailingCommas = true;
+    //反序列化过程中属性名称是否使用不区分大小写的比较
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = false;
+})
+// Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+// 解决Json 转换问题,例如：前台传时间后台转换对象时中间空格需要变为T
+// 用[JsonProperty("role_id")] 而不要用[JsonPropertyName("id")] 
+.AddNewtonsoftJson();
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+// builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add Entity Framework Core services for Oracle database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
 	options.UseOracle(
-		builder.Configuration.GetConnectionString("DefaultConnection"),
+		builder.Configuration.GetConnectionString("WaiMaiDBCon"),
 		o => o.MigrationsAssembly("takeout_tj"));
+});
+
+// 在Program.cs注册上下文并指定数据库连接字符串
+// builder.Services.AddDbContext<ModelContext>(option => option.UseOracle(builder.Configuration.GetConnectionString("WaiMaiDBCon")));
+
+builder.Services.AddSwaggerGen();
+
+// 测试环境或开发环境下, 任何跨域请求都通过
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
+// 允许跨域
+app.UseCors("AllowAllOrigins");
 
-app.UseRouting();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
