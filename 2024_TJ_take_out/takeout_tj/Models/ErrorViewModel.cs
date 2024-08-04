874657115539cfa9e6f1fc38d1dc58bb12937dcf
JiangShiYu6 <129039574+JiangShiYu6@users.noreply.github.com>
2024-08-05 01:46:09 +0800

    Delete 2024_TJ_take_out directory

--- 2024_TJ_take_out/takeout_tj/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace takeout_tj.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
