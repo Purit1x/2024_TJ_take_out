3852f438dae4237ba5f1f4f9b332b80fbdbc3365
1606599515 <167835133+1606599515@users.noreply.github.com>
2024-08-10 19:44:33 +0800

    后端 8-10最新版
    1.后端实现注册后自动分配Id，并修改注册输出逻辑，使前端可以读取id
    2.更改商家营业时间TimeforOpenBusiness和TimeforCloseBusiness为int类型，表示24小时中的秒数
    3.缺乏appsettings.json，下载后要加进去

---
+++ Models/Merchant/DishDB.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using takeout_tj.Models.Platform;
+using takeout_tj.Models.User;
+
+namespace takeout_tj.Models.Merchant
+{
+	public class DishDB
+	{
+		[Required(ErrorMessage = "Merchant ID is required. ")]
+		public int MerchantId { get; set; }
+
+		[Required(ErrorMessage = "Dish ID is required. ")]
+		public int DishId { get; set; }
+
+		[Required(ErrorMessage = "Dish name is required. ")]
+		[StringLength(255,ErrorMessage = "Dish name must be at most 15 characters long. ")]
+		public string DishName { get; set; }
+
+		[Required(ErrorMessage = "Dish price is required. ")]
+		public decimal DishPrice { get; set; }
+
+		public string DishCategory { get; set; }  // 菜品列别, 可以为空
+
+		public MerchantDB MerchantDB { get; set; }
+		public ICollection<ShoppingCartDB> ShoppingCartDBs { get; set; }
+		public ICollection<OrderDishDB> OrderDishDBs { get; set; }
+	}
+}
