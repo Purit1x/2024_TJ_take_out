394ab6e2eecfd79208c647e1da0301950a891b6b
1606599515 <167835133+1606599515@users.noreply.github.com>
2024-08-16 11:02:36 +0800

    后端 8.16 11:00
    在Users中添加接口：
    1.增加接口userSearch根据Id查找用户信息
    2.增加接口userEdit用于编辑用户信息
    3.增加接口recharge用于用户充值
    4.增加接口GetMerchantIds用于获取所有的商家Id
    5.增加接口merchantsSearch根据id数组搜索所有可见商家信息
    6.增加接口CreateFM用于创建收藏
    7.增加接口getFM用于获取用户收藏
    8.增加接口deleteFM用于删除用户收藏

---
+++ takeout_tj/DTO/FavoriteMerchantDBDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace takeout_tj.DTO
+{
+    public class FavoriteMerchantDBDto
+    {
+        public int UserId { get; set; }
+        public int MerchantId { get; set; }
+    }
+}
