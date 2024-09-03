f787b5273dd1f3f100b73af0991616739a0ec8b6
Purit1x <xuy_eason@foxmail.com>
2024-09-04 01:48:34 +0800

    Fix merge bugs.

--- vue_wm_app/src/views/platform/MerchantManage.vue
+++ vue_wm_app/src/views/platform/MerchantManage.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { useStore } from "vuex";
-import { ElMessage,ElMessageBox } from "element-plus";
-import { ref, onMounted } from 'vue';  
+import { ElMessage, ElMessageBox } from "element-plus";
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { getMerchantIds,getMerchantsInfo,getAllMerchantsInfo } from "@/api/user";
+import { getMerchantIds, getMerchantsInfo, getAllMerchantsInfo } from "@/api/user";
 import { merchantDeleteService } from "@/api/platform";
 import { getMerAvgRating } from "@/api/merchant";
 const router = useRouter();
@@ -11,16 +11,16 @@ const merchantIds = ref([]);  //获取所有商家id
 const merchantsInfo = ref([]); // 存储所有商家信息 
 const showMerchantsInfo = ref([]); // 显示商家信息列表
 const searchQuery = ref(''); // 搜索框内容
-const isMerchantInfo= ref(false); // 是否显示商家信息
+const isMerchantInfo = ref(false); // 是否显示商家信息
 const currentMerchantId = ref(null); // 当前商家id
-const MerchantInfo=ref({});  //商户信息
+const MerchantInfo = ref({});  //商户信息
 
 const sortField = ref(''); // 搜索框内容
 const sortOrder = ref('1'); // 搜索框内容
-const parentBorder=ref(false);
-const childBorder=ref(false);
-onMounted(() => {
-    
+const parentBorder = ref(false);
+const childBorder = ref(false);
+onMounted(async () => {
+
     // getMerchantIds().then(res => {  // 获取所有商家id
     //     merchantIds.value = res.data;  
     //     return Promise.all(merchantIds.value.map(id => getMerchantsInfo(id))); // 并发请求所有商家信息
@@ -30,76 +30,76 @@ onMounted(() => {
     // }).catch(err => {  
     //     ElMessage.error('获取商家id失败'); 
     // }); 
-    getMerchantIds().then(res => { 
-    merchantIds.value = res.data;  
+    getMerchantIds().then(res => {
+        merchantIds.value = res.data;
     });
-    getAllMerchantsInfo().then(res=>{
-        merchantsInfo.value  = res.data;
+    getAllMerchantsInfo().then(res => {
+        merchantsInfo.value = res.data;
         showMerchantsInfo.value = merchantsInfo.value;
         // fetchDefaultAddress();
     })
     await fetchMerAvgRating();
-});
+})
 
 const sortCoupons = () => {
-  getAllMerchantsInfo(sortField.value,sortOrder.value).then(res=>{
-    merchantsInfo.value  = res.data;
-    showMerchantsInfo.value = merchantsInfo.value;
-  })
-
+    getAllMerchantsInfo(sortField.value, sortOrder.value).then(res => {
+        merchantsInfo.value = res.data;
+        showMerchantsInfo.value = merchantsInfo.value;
+    })
+}
 const fetchMerAvgRating = async () => {
-  try {
-    for (let info of showMerchantsInfo.value) {
-      const avgRating = await getMerAvgRating(info.merchantId);
-      console.log(`Merchant ID: ${info.merchantId}, Avg Rating: ${avgRating}`);
-      info.avgRating = avgRating; // 将平均评分添加到商家信息对象中
+    try {
+        for (let info of showMerchantsInfo.value) {
+            const avgRating = await getMerAvgRating(info.merchantId);
+            console.log(`Merchant ID: ${info.merchantId}, Avg Rating: ${avgRating}`);
+            info.avgRating = avgRating; // 将平均评分添加到商家信息对象中
+        }
+        console.log("商家信息", showMerchantsInfo.value);
+    } catch (error) {
+        console.error('Failed to fetch merchant average ratings:', error);
     }
-    console.log("商家信息", showMerchantsInfo.value);
-  } catch (error) {
-    console.error('Failed to fetch merchant average ratings:', error);
-  }
 
 };
-const formatTime=(seconds)=> {  
-    const hours = Math.floor(seconds / 3600);  
-    const minutes = Math.floor((seconds % 3600) / 60);  
-    const secs = seconds % 60;  
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;  
-}  
+const formatTime = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+}
 const handleSearch = () => {   //字符串匹配搜索商家
-    const query = searchQuery.value.trim();  
-    if (query) {  
-        showMerchantsInfo.value = merchantsInfo.value.filter(merchant =>  
-            merchant.merchantName.includes(query) ||  
-            merchant.dishType.includes(query)  
-        );  
-    } else {  
-        showMerchantsInfo.value = merchantsInfo.value;  
+    const query = searchQuery.value.trim();
+    if (query) {
+        showMerchantsInfo.value = merchantsInfo.value.filter(merchant =>
+            merchant.merchantName.includes(query) ||
+            merchant.dishType.includes(query)
+        );
+    } else {
+        showMerchantsInfo.value = merchantsInfo.value;
     }
-};  
+};
 const enterMerchantInfo = (merchantId) => {  // 进入商家管理页面
     currentMerchantId.value = merchantId;
     MerchantInfo.value = merchantsInfo.value.find(merchant => merchant.merchantId === merchantId);
-    MerchantInfo.value.timeforOpenBusiness=formatTime(MerchantInfo.value.timeforOpenBusiness);  //时间转换
-    MerchantInfo.value.timeforCloseBusiness=formatTime(MerchantInfo.value.timeforCloseBusiness);
+    MerchantInfo.value.timeforOpenBusiness = formatTime(MerchantInfo.value.timeforOpenBusiness);  //时间转换
+    MerchantInfo.value.timeforCloseBusiness = formatTime(MerchantInfo.value.timeforCloseBusiness);
     isMerchantInfo.value = true;
 };
 const deleteMerchant = (merchantId) => {  // 销号
-    ElMessageBox.confirm('确认要删除该商家吗?', '提示', {  
-        confirmButtonText: '确定',  
-        cancelButtonText: '取消',  
-        type: 'warning'  
-    }).then(() => {  
-        merchantDeleteService(merchantId).then(res => {  
-            ElMessage.success('销号成功');  
-            showMerchantsInfo.value = showMerchantsInfo.value.filter(merchant => merchant.merchantId!== merchantId);  
-            isMerchantInfo.value = false;  
-            currentMerchantId.value = null;  
-            MerchantInfo.value = {};  
-        }).catch(err => {  
-            ElMessage.error('销号失败');  
-        });  
-    }).catch(() => {});  
+    ElMessageBox.confirm('确认要删除该商家吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+    }).then(() => {
+        merchantDeleteService(merchantId).then(res => {
+            ElMessage.success('销号成功');
+            showMerchantsInfo.value = showMerchantsInfo.value.filter(merchant => merchant.merchantId !== merchantId);
+            isMerchantInfo.value = false;
+            currentMerchantId.value = null;
+            MerchantInfo.value = {};
+        }).catch(err => {
+            ElMessage.error('销号失败');
+        });
+    }).catch(() => { });
 };
 const gobackHome = () => {
     router.push('/platform-home');
@@ -166,37 +166,34 @@ const gobackHome = () => {
             商家管理
             <button @click="gobackHome">返回</button>
         </div>
-        <h2>商家列表</h2>  
+        <h2>商家列表</h2>
         <div>
-            <input type="text" v-model="searchQuery" placeholder="搜索店名或类别" v-on:keyup.enter="handleSearch()"/> 
+            <input type="text" v-model="searchQuery" placeholder="搜索店名或类别" v-on:keyup.enter="handleSearch()" />
             <button @click="handleSearch()">搜索</button>
         </div>
-    
-            <el-table :data="showMerchantsInfo":border="parentBorder"style="width:100%">
+
+        <el-table :data="showMerchantsInfo" :border="parentBorder" style="width:100%">
             <el-table-column type="expand">
                 <template #default="props">
                     <div m="4">
                         <p m="t-0 b-2">商家信息</p>
                         <p m="t-0 b-2">地址：{{ props.row.merchantAddress }}</p>
-                        <p m="t-0 b-2">营业时间：{{formatTime(props.row.timeforOpenBusiness)}}-{{ formatTime(props.row.timeforCloseBusiness) }}</p>
+                        <p m="t-0 b-2">营业时间：{{ formatTime(props.row.timeforOpenBusiness) }}-{{
+                            formatTime(props.row.timeforCloseBusiness) }}</p>
                         <p m="t-0 b-2">联系电话：{{ props.row.contact }}</p>
                         <p m="t-0 b-2">是否可以使用通用优惠券：{{ props.row.couponType ? '否' : '是' }}</p>
                         <p m="t-0 b-2">商家评分：{{ props.row.avgRating }}</p>
                         <button @click="deleteMerchant(currentMerchantId)">销号</button>
-                
+
                     </div>
                 </template>
 
             </el-table-column>
             <el-table-column label="商家" prop="merchantName" />
             <el-table-column label="商品类别" prop="dishType" />
-            </el-table>
+        </el-table>
     </div>
 
 
 </template>
-<style scoped>
-
-
-</style>
-
+<style scoped></style>
--- vue_wm_app/src/views/user/UserHome.vue
+++ vue_wm_app/src/views/user/UserHome.vue
@@ -1,21 +1,21 @@
-<script setup>  
-import { ref, onMounted, watch, onBeforeUnmount } from 'vue';  
+<script setup>
+import { ref, onMounted, watch, onBeforeUnmount } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from "vuex";
 import { provide } from 'vue';
 
-import { getMerchantIds,getMerchantsInfo,createFavouriteMerchant,GetDefaultAddress,GetUserAddress, getAllMerchantsInfo } from "@/api/user";
-import {getDistanceBetweenAddresses,getMerAvgRating, GetMultiSpecialOffer} from "@/api/merchant";
+import { getMerchantIds, getMerchantsInfo, createFavouriteMerchant, GetDefaultAddress, GetUserAddress, getAllMerchantsInfo } from "@/api/user";
+import { getDistanceBetweenAddresses, getMerAvgRating, GetMultiSpecialOffer } from "@/api/merchant";
 
 import { ElMessage } from 'element-plus';
 
-const store = useStore();    
+const store = useStore();
 const router = useRouter();
 const user = ref({}); // 初始化用户信息对象  
 const isUserHome = ref(true); // 页面是否为用户主页 
 const merchantIds = ref([]);  //获取所有商家id
 const merchantsInfo = ref([]); // 存储所有商家信息 
-const isMenu= ref(false); // 是否在看菜单
+const isMenu = ref(false); // 是否在看菜单
 const searchQuery = ref(''); // 搜索框内容
 
 const sortField = ref(''); // 搜索框内容
@@ -27,45 +27,45 @@ const filterSpecialOfferQuery = ref(false); // 满减筛选内容(筛选用)
 
 const showMerchantsInfo = ref([]); // 显示商家信息列表
 const hasDefaultAddress = ref(true); // 用于跟踪是否有默认地址
-const DefaultAddress=ref(null); // 用于跟踪默认地址
-const DefaultAddressId=ref(null); // 用于跟踪默认地址id
+const DefaultAddress = ref(null); // 用于跟踪默认地址
+const DefaultAddressId = ref(null); // 用于跟踪默认地址id
 let updateAvgRatingItv = null;
-onMounted(async() => {  
-  const userData = store.state.user; 
-  if(router.currentRoute.value.path !== '/user-home')
+onMounted(async () => {
+  const userData = store.state.user;
+  if (router.currentRoute.value.path !== '/user-home')
     isUserHome.value = false;
   else
-    isUserHome.value = true; 
-  if (userData) {  
-    user.value = userData;  
-  } else {  
+    isUserHome.value = true;
+  if (userData) {
+    user.value = userData;
+  } else {
     router.push('/login');
-  }  
-  
-  getMerchantIds().then(res => { 
-    merchantIds.value = res.data;  
+  }
+
+  getMerchantIds().then(res => {
+    merchantIds.value = res.data;
   });
-  getAllMerchantsInfo().then(res=>{
-    merchantsInfo.value  = res.data;
+  getAllMerchantsInfo().then(res => {
+    merchantsInfo.value = res.data;
     fetchDefaultAddress();
   });
   await fetchMerAvgRating();
-  updateAvgRatingItv = setInterval(fetchMerAvgRating,5000);
+  updateAvgRatingItv = setInterval(fetchMerAvgRating, 5000);
 
-}); 
+});
 
 onBeforeUnmount(() => {
-  if(updateAvgRatingItv)
+  if (updateAvgRatingItv)
     clearInterval(updateAvgRatingItv);
 })
 
 
 const sortCoupons = () => {
-  getAllMerchantsInfo(sortField.value,sortOrder.value).then(res=>{
-    merchantsInfo.value  = res.data;
+  getAllMerchantsInfo(sortField.value, sortOrder.value).then(res => {
+    merchantsInfo.value = res.data;
     fetchDefaultAddress();
   })
-}); 
+}
 const fetchMerAvgRating = async () => {
   try {
     for (let info of showMerchantsInfo.value) {
@@ -79,121 +79,121 @@ const fetchMerAvgRating = async () => {
   }
 
 };
-const fetchDefaultAddress = async () => {  
-    try {  
-        const res = await GetDefaultAddress(user.value.userId); // 获取用户默认地址  
-        if (res.data !== 'none') {  
-            hasDefaultAddress.value = true;  
-            DefaultAddressId.value = res.data;  
+const fetchDefaultAddress = async () => {
+  try {
+    const res = await GetDefaultAddress(user.value.userId); // 获取用户默认地址  
+    if (res.data !== 'none') {
+      hasDefaultAddress.value = true;
+      DefaultAddressId.value = res.data;
 
-            // 获取默认地址  
-            const addressRes = await GetUserAddress(DefaultAddressId.value);  
-            DefaultAddress.value = addressRes.data;  
-            console.log(DefaultAddress.value);  
+      // 获取默认地址  
+      const addressRes = await GetUserAddress(DefaultAddressId.value);
+      DefaultAddress.value = addressRes.data;
+      console.log(DefaultAddress.value);
 
-            if (hasDefaultAddress.value && DefaultAddress.value) {  
-                // 计算每个商家与默认地址之间的距离  
-                for (const merchant of merchantsInfo.value) {  
-                    // 假设商家的地址为 merchant.merchantAddress  
-                    const distance = await getDistanceBetweenAddresses(merchant.merchantAddress, DefaultAddress.value)/1000;  
-                    // 将计算的距离添加到商家信息中，单位为米，并保留一位小数  
-                    merchant.distanceFromDefaultAddress = distance ? (distance * 1000).toFixed(1) : 0;  
-                }  
-                // merchantsInfo.value.sort((a, b) => {  //升序排列
-                //     const distanceA = parseFloat(a.distanceFromDefaultAddress) || 0;  
-                //     const distanceB = parseFloat(b.distanceFromDefaultAddress) || 0;  
-                //     return distanceA - distanceB;  
-                // });  
-            }  
-            console.log(merchantsInfo.value);  
-            showMerchantsInfo.value = merchantsInfo.value; // 显示商家信息列表  
-        } else {  
-            hasDefaultAddress.value = false;  
-            ElMessage.error('请设置默认地址');  
-        }  
-    } catch (err) {  
-        ElMessage.error('请设置默认地址'); // 处理获取默认地址以及其他潜在错误  
-        console.error(err); // 可选：记录错误以便调试  
-    }  
-};  
-watch(  
-    () => router.currentRoute.value.path,  
-    (newPath) => {  
-        // 检查当前路由是否是用户主页或商家菜单  
-        fetchDefaultAddress();
-        if (newPath.startsWith('/user-home') &&   
-            newPath !== '/user-home/personal'&&
-            (newPath !== '/user-home/cart')&&
-            (newPath !== '/user-home/address')&&
-            (newPath !== '/user-home/personal/coupon')&&
-            (newPath !== '/user-home/personal/coupon/couponPurchase')&&
-            (newPath !== '/user-home/personal/myOrder')) {  
-            isUserHome.value = !newPath.startsWith('/user-home/merchant/'); // 如果是商家菜单，设置为 false  
-        } else {  
-            isUserHome.value = false;   
-        }   
-        localStorage.setItem('isUserHome', isUserHome.value);  
-    }  
-);   
+      if (hasDefaultAddress.value && DefaultAddress.value) {
+        // 计算每个商家与默认地址之间的距离  
+        for (const merchant of merchantsInfo.value) {
+          // 假设商家的地址为 merchant.merchantAddress  
+          const distance = await getDistanceBetweenAddresses(merchant.merchantAddress, DefaultAddress.value) / 1000;
+          // 将计算的距离添加到商家信息中，单位为米，并保留一位小数  
+          merchant.distanceFromDefaultAddress = distance ? (distance * 1000).toFixed(1) : 0;
+        }
+        // merchantsInfo.value.sort((a, b) => {  //升序排列
+        //     const distanceA = parseFloat(a.distanceFromDefaultAddress) || 0;  
+        //     const distanceB = parseFloat(b.distanceFromDefaultAddress) || 0;  
+        //     return distanceA - distanceB;  
+        // });  
+      }
+      console.log(merchantsInfo.value);
+      showMerchantsInfo.value = merchantsInfo.value; // 显示商家信息列表  
+    } else {
+      hasDefaultAddress.value = false;
+      ElMessage.error('请设置默认地址');
+    }
+  } catch (err) {
+    ElMessage.error('请设置默认地址'); // 处理获取默认地址以及其他潜在错误  
+    console.error(err); // 可选：记录错误以便调试  
+  }
+};
+watch(
+  () => router.currentRoute.value.path,
+  (newPath) => {
+    // 检查当前路由是否是用户主页或商家菜单  
+    fetchDefaultAddress();
+    if (newPath.startsWith('/user-home') &&
+      newPath !== '/user-home/personal' &&
+      (newPath !== '/user-home/cart') &&
+      (newPath !== '/user-home/address') &&
+      (newPath !== '/user-home/personal/coupon') &&
+      (newPath !== '/user-home/personal/coupon/couponPurchase') &&
+      (newPath !== '/user-home/personal/myOrder')) {
+      isUserHome.value = !newPath.startsWith('/user-home/merchant/'); // 如果是商家菜单，设置为 false  
+    } else {
+      isUserHome.value = false;
+    }
+    localStorage.setItem('isUserHome', isUserHome.value);
+  }
+);
 // 返回主页函数
 const gobackHome = () => {
 
   router.push('/user-home');
 };
 // 跳转到个人信息  
-const goToPersonal = () => { 
-    router.push('/user-home/personal');  
-    isUserHome.value = false; // 进入个人信息页面时隐藏欢迎信息和按钮  
-};  
-const enterDishes = (id) => {  
+const goToPersonal = () => {
+  router.push('/user-home/personal');
+  isUserHome.value = false; // 进入个人信息页面时隐藏欢迎信息和按钮  
+};
+const enterDishes = (id) => {
   isUserHome.value = false; // 进入商家详情页面时隐藏欢迎信息和按钮  
-  router.push('/user-home/merchant/' + id);  
-};  
-const goToCart = () => {  
+  router.push('/user-home/merchant/' + id);
+};
+const goToCart = () => {
   isUserHome.value = false; // 进入购物车页面时隐藏欢迎信息和按钮  
-  router.push('/user-home/cart');  
-};  
-const goToAddress = () => {  
+  router.push('/user-home/cart');
+};
+const goToAddress = () => {
   isUserHome.value = false; // 进入地址页面时隐藏欢迎信息和按钮  
-  router.push('/user-home/address');  
-};  
-const addToFavorite = async (merchantId) => {  
-    const userId = user.value.userId; // 从用户对象中获取用户 ID  
-    try {  
-        const data = {    
-            UserId: userId,  
-            MerchantId: merchantId  
-        };  
+  router.push('/user-home/address');
+};
+const addToFavorite = async (merchantId) => {
+  const userId = user.value.userId; // 从用户对象中获取用户 ID  
+  try {
+    const data = {
+      UserId: userId,
+      MerchantId: merchantId
+    };
 
-        const response = await createFavouriteMerchant(data); // 调用 API 创建收藏商户  
-        ElMessage.success(response.msg); // 显示成功消息  
-    } catch (error) {  
-      if (error.response && error.response.data) {  
-        const errorCode = error.response.data.errorCode;  
+    const response = await createFavouriteMerchant(data); // 调用 API 创建收藏商户  
+    ElMessage.success(response.msg); // 显示成功消息  
+  } catch (error) {
+    if (error.response && error.response.data) {
+      const errorCode = error.response.data.errorCode;
 
-        if (errorCode === 20000) {  
-         ElMessage.error('没有更新数据');  
-        } else if (errorCode === 30000) {  
-          ElMessage.error('请勿重复收藏');  
-          } else {  
-            ElMessage.error('发生未知错误');  
-            }  
-        } else {  
-          ElMessage.error('网络错误，请重试');  
-        }  
-    }  
-};  
-const handleSearch = () => {   //字符串匹配搜索商家
-  const query = searchQuery.value.trim();  
-  if (query) {  
-    showMerchantsInfo.value = merchantsInfo.value.filter(merchant =>  
-      merchant.merchantName.includes(query) ||  
-      merchant.dishType.includes(query)  
-    );  
-  } else {  
-    showMerchantsInfo.value = merchantsInfo.value;  
+      if (errorCode === 20000) {
+        ElMessage.error('没有更新数据');
+      } else if (errorCode === 30000) {
+        ElMessage.error('请勿重复收藏');
+      } else {
+        ElMessage.error('发生未知错误');
+      }
+    } else {
+      ElMessage.error('网络错误，请重试');
+    }
   }
-};  
+};
+const handleSearch = () => {   //字符串匹配搜索商家
+  const query = searchQuery.value.trim();
+  if (query) {
+    showMerchantsInfo.value = merchantsInfo.value.filter(merchant =>
+      merchant.merchantName.includes(query) ||
+      merchant.dishType.includes(query)
+    );
+  } else {
+    showMerchantsInfo.value = merchantsInfo.value;
+  }
+};
 const filteredMerchants = async () => {
   const query_address = filterAddressQuery.value.trim();
   const query_couponType = filterCouponTypeQuery.value;
@@ -211,35 +211,35 @@ const filteredMerchants = async () => {
     });
   } else {
     showMerchantsInfo.value = merchantsInfo.value;
-  } 
+  }
 };
 // 提供 user 对象 给其它子网页 
-provide('user', user); 
+provide('user', user);
 provide('merchantsInfo', merchantsInfo); 
-</script>  
+</script>
 
 <template>
   <nav class="sidebar">
     <div class="sidebar-content">
-      <img class="sidebar-img" src="@/assets/my_logo.png" alt="logo"/>
+      <img class="sidebar-img" src="@/assets/my_logo.png" alt="logo" />
 
       <button class="sidebar-button" @click="gobackHome">
-        <img src="@/assets/merchant_home.png" alt="主页"/>
+        <img src="@/assets/merchant_home.png" alt="主页" />
         <span class="button-text">主页</span>
       </button>
 
       <button class="sidebar-button" @click="goToCart">
-        <img src="@/assets/user_order.png" alt="购物车"/>
+        <img src="@/assets/user_order.png" alt="购物车" />
         <span class="button-text">购物车</span>
       </button>
 
       <button class="sidebar-button" @click="goToPersonal">
-        <img src="@/assets/merchant_personal.png" alt="个人信息"/>
+        <img src="@/assets/merchant_personal.png" alt="个人信息" />
         <span class="button-text">个人信息</span>
       </button>
 
       <button class="sidebar-button" @click="goToAddress">
-        <img src="@/assets/address.png" alt="地址"/>
+        <img src="@/assets/address.png" alt="地址" />
         <span class="button-text">地址</span>
       </button>
       <router-view /> <!-- 渲染子路由 -->
@@ -255,10 +255,13 @@ provide('merchantsInfo', merchantsInfo);
     <div v-if="isUserHome">
       <div class="search-bar">
         <el-col :span="16">
-          <el-input placeholder="搜索店名或类别" v-model="searchQuery" clearable @clear="handleSearch" @keydown.enter.native="handleSearch">
+          <el-input placeholder="搜索店名或类别" v-model="searchQuery" clearable @clear="handleSearch"
+            @keydown.enter.native="handleSearch">
             <template #append>
               <el-button type="primary" @click="handleSearch" class="search-button">
-                <el-icon><search /></el-icon>
+                <el-icon>
+                  <search />
+                </el-icon>
               </el-button>
             </template>
           </el-input>
@@ -266,7 +269,8 @@ provide('merchantsInfo', merchantsInfo);
       </div>
 
       <div class="filter-bar">
-        <el-input v-model="filterAddressQuery" placeholder="筛选地址" clearable @keydown.enter="filteredMerchants()" class="filter-input"></el-input>
+        <el-input v-model="filterAddressQuery" placeholder="筛选地址" clearable @keydown.enter="filteredMerchants()"
+          class="filter-input"></el-input>
         <el-checkbox v-model="filterCouponTypeQuery" class="filter-checkbox">可使用优惠券</el-checkbox>
         <el-checkbox v-model="filterSpecialOfferQuery" class="filter-checkbox">正在特惠中</el-checkbox>
         <el-button type="primary" @click="filteredMerchants()" class="filter-button">筛选</el-button>
@@ -311,12 +315,13 @@ body {
 </style>
 
 <style scoped lang="scss">
-table{
+table {
   width: 100%;
   border-collapse: collapse;
 }
 
-th, td {
+th,
+td {
   border: 1px solid #ddd;
   padding: 8px;
   text-align: center;
@@ -331,9 +336,11 @@ th {
 
 /* 侧边栏样式 */
 .sidebar {
-  width: 80px; /* 调整侧边栏宽度为50px */
+  width: 80px;
+  /* 调整侧边栏宽度为50px */
   background: linear-gradient(to bottom, #f0d6f2, #f77dd048);
-  padding: 20px 0; /* 调整为顶部和底部填充 */
+  padding: 20px 0;
+  /* 调整为顶部和底部填充 */
   height: 100vh;
   position: fixed;
   top: 0;
@@ -344,7 +351,8 @@ th {
 }
 
 .sidebar-img {
-  width: 80%; /* 根据新宽度调整logo大小 */
+  width: 80%;
+  /* 根据新宽度调整logo大小 */
   height: auto;
   margin-bottom: 15px;
 }
@@ -360,18 +368,21 @@ th {
   border: none;
   cursor: pointer;
   transition: background-color 0.3s;
-  flex-direction: column; /* 改为列方向布局，使图标在上，文字在下 */
+  flex-direction: column;
+  /* 改为列方向布局，使图标在上，文字在下 */
 }
 
 .sidebar-button img {
-  width: 30px; /* 调整按钮图片大小 */
+  width: 30px;
+  /* 调整按钮图片大小 */
   height: auto;
 }
 
 /* 头部和欢迎信息 */
 .content-header {
   margin-bottom: 20px;
-  margin-left: 40px; /* 调整左边距，避免被侧边栏遮挡 */
+  margin-left: 40px;
+  /* 调整左边距，避免被侧边栏遮挡 */
 }
 
 .welcome-text {
@@ -383,10 +394,13 @@ th {
 /* 搜索栏样式 */
 .search-bar {
   display: flex;
-  justify-content: flex-start; /* 将搜索栏从左侧对齐 */
-  margin-left: 40px; /* 调整左边距 */
+  justify-content: flex-start;
+  /* 将搜索栏从左侧对齐 */
+  margin-left: 40px;
+  /* 调整左边距 */
   margin-bottom: 20px;
-  width: calc(50% - 60px); /* 调整搜索栏宽度 */
+  width: calc(50% - 60px);
+  /* 调整搜索栏宽度 */
 }
 
 .search-bar .el-input {
@@ -412,21 +426,26 @@ th {
 .filter-bar {
   display: flex;
   align-items: center;
-  justify-content: flex-start; /* 将筛选栏从左侧对齐 */
-  margin-left: 40px;/* 调整左边距 */
+  justify-content: flex-start;
+  /* 将筛选栏从左侧对齐 */
+  margin-left: 40px;
+  /* 调整左边距 */
   margin-bottom: 20px;
-  width: 100%; /* 使筛选栏宽度占满容器 */
+  width: 100%;
+  /* 使筛选栏宽度占满容器 */
 }
 
 .filter-input {
   width: 250px;
   border-radius: 25px;
   background-color: #faf0f0;
-  margin-right: 15px; /* 给输入框添加右边距 */
+  margin-right: 15px;
+  /* 给输入框添加右边距 */
 }
 
 .filter-checkbox {
-  margin-right: 15px; /* 给复选框添加右边距 */
+  margin-right: 15px;
+  /* 给复选框添加右边距 */
   font-size: 16px;
   color: #555;
 }
@@ -448,11 +467,14 @@ th {
 
 /* 表格样式 */
 .styled-table {
-  width: calc(100% - 80px); /* 调整表格宽度，考虑侧边栏的宽度 */
+  width: calc(100% - 80px);
+  /* 调整表格宽度，考虑侧边栏的宽度 */
   border-collapse: collapse;
   max-width: 1300px;
-  margin-left: 40px; /* 调整表格左边距 */
-  margin-right: 40px; /* 调整表格右边距 */
+  margin-left: 40px;
+  /* 调整表格左边距 */
+  margin-right: 40px;
+  /* 调整表格右边距 */
   margin-bottom: 20px;
   font-size: 16px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
@@ -488,7 +510,8 @@ th {
 }
 
 
-.col-enter button, .col-favorite button {
+.col-enter button,
+.col-favorite button {
   padding: 6px 12px;
   border-radius: 20px;
   background-color: #ff69b4;
@@ -498,7 +521,8 @@ th {
   transition: background-color 0.3s, box-shadow 0.3s;
 }
 
-.col-enter button:hover, .col-favorite button:hover {
+.col-enter button:hover,
+.col-favorite button:hover {
   background-color: #ff85c1;
   box-shadow: 0 0 8px rgba(255, 105, 180, 0.8);
 }
